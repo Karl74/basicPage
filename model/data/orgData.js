b583ef75fcffbb71358cb2ebc074120762578c31
Karl74 <carlos.castro74@gmail.com>
2019-04-14 13:59:05 -0500

    OrgData module

---
+++ model/data/orgData.js
@@ -0,0 +1,47 @@
+const OrgData = {
+	orgName:"This Small Church",
+	moto:"God, Friends and Food",
+	section: [
+		{
+			secName: "Services",
+			imageUrl:"./this location",
+			info:" all this information"
+		},
+
+		{
+			secName: "Calendar",
+			imageUrl:"./this location",
+			info:" all this information"
+		},
+
+		{
+			secName: "Clubs",
+			imageUrl:"./this location",
+			info:" all this information"
+		},
+
+		{
+			secName: "Learning",
+			imageUrl:"./this location",
+			info:" all this information"
+		},
+
+		{
+			secName: "Directory",
+			imageUrl:"./this location",
+			info:" all this information"
+		}
+			],
+	Leadership:{
+			leaderName:"Jhon Doe",
+			leaderQuote: "this is a long string",
+			email: "the.leader@mail.net"
+		},
+	generalInfo: {
+		adress:"this is the org addres",
+		phone:"727-127-2323",
+	}
+	
+}
+
+export default OrgData
--- view/components/Header.js
+++ view/components/Header.js
@@ -1,4 +1,6 @@
 import React from "react"
+import OrgData from "../../model/data/orgData"
+
 const h1Style = {
   fontFamily: '"Century Gothic", CenturyGothic, "AppleGothic", sans-serif',
   fontSize: "30px",
@@ -9,14 +11,18 @@ const h1Style = {
   // backgroundColor: "#e7e5e4",
   letterSpacing: ".15em",
   textShadow: "1px -1px 0 #767676, -1px 2px 1px #737272,-2px 4px 1px #767474, -3px 6px 1px #787777,-4px 8px 1px #7b7a7a" 
-  
-}
+  }
+
+const {orgName} = OrgData
+const {moto} = OrgData
 
 const Header = ()=>
 		<div>
-      <h1 style={h1Style}> This Small Church </h1>
-      <h3> God, Friends and Food</h3>
-		</div>
+      <h1 style={h1Style}> {orgName}  </h1>
+		
+      <h3>{moto}</h3>
+    
+    </div>
 	
 
 export default Header	
